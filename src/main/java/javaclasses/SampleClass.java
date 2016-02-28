9923d1520fca222002b577d72d74443551da85c2
adamhurwitz <adamscotthurwitz@gmail.com>
2016-02-28 05:28:51 -0800

    Solved

--- src/main/java/javaclasses/SampleClass.java
+++ src/main/java/javaclasses/SampleClass.java
@@ -6,16 +6,62 @@ package javaclasses;
 public class SampleClass {
     static private final String LOG_TAG = SampleClass.class.getSimpleName();
 
+    static int[] array = {22, 11, 99, 88, 9, 7, 42};
+    //static int minValueIndex;
+    int minValue;
+
     public static void main(String[] arg) {
-        System.out.println("Answer is: " + stringMethod("Hello", "World"));
+        System.out.println("Answer is: " + sortArrayMethod(array));
     }
 
-    static int[] swapArray = {7, 9, 4};
+    public static int[] sortArrayMethod(int[] array) {
+        int[] sortedArray = array;
+        //System.out.println("sortedArray.length - " + sortedArray.length);
 
-    public static String stringMethod(String firstWord, String secondWord) {
-        String stringToPrint;
-        stringToPrint = firstWord + " " + secondWord;
+        System.out.print("sortedArray - ");
+        for (int x = 0; x < sortedArray.length; x++) {
+            System.out.print(sortedArray[x] + " | ");
+        }
+        System.out.println("");
 
-        return stringToPrint;
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.println("i - " + i + " swap(" + i + ","
+                    + " findIndexofMinimum of " + findIndexOfMinimum(i) + ")");
+            swap(i, findIndexOfMinimum(i));
+
+            /*System.out.println("start at - " + i + " findIndexOfMinimum - " +
+                    array[findIndexOfMinimum(i)]);
+            //minValueIndex = array[findIndexOfMinimum(i)];
+            swap(array[i], array[findIndexOfMinimum(i)]);*/
+        }
+        return sortedArray;
+    }
+
+    public static int findIndexOfMinimum(int indexToStart) {
+        int minValIndex = indexToStart;
+        int minVal = array[indexToStart];
+        //System.out.println("minValIndex - " + indexToStart + " minVal - " + array[indexToStart]);
+        for (int j = minValIndex; j < array.length; j++) {
+            // System.out.println("j - " + j);
+            if (array[j] < minVal) {
+                minValIndex = j;
+                minVal = array[j];
+            }
+        }
+        //System.out.println("minValIndex - " + minValIndex);
+        return minValIndex;
+    }
+
+    public static void swap(int swapOneIndex, int swapTwoIndex) {
+        int temp = array[swapOneIndex];
+        array[swapOneIndex] = array[swapTwoIndex];
+        array[swapTwoIndex] = temp;
+
+        System.out.print("sortedArray - ");
+        for (int x = 0; x < array.length; x++) {
+            System.out.print(array[x] + " | ");
+        }
+        System.out.println("");
+
     }
 }
